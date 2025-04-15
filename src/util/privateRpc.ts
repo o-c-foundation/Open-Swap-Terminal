daedc3cca3ef2520e415702022bac9961de127ce
thecoder223 <100096442+thecoder223@users.noreply.github.com>
2025-04-15 03:10:55 -0400

    Switch to Alchemy RPC endpoint

--- src/util/privateRpc.ts
+++ src/util/privateRpc.ts
@@ -1,5 +1,6 @@
 import { Connection } from "@solana/web3.js";
 
+// Use Alchemy RPC endpoint as specified by the user
 export const privateConnection = new Connection(
-  "https://solana-mainnet.core.chainstack.com/ff2a1adced524a99c90cc768cdfb889f"
+  "https://solana-mainnet.g.alchemy.com/v2/EoZ8ZFsGk-Xgn1AftQvuigvKKIHIyZcI"
 );
